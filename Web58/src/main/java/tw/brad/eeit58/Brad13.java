75f3cf4923fa424b8bf723e7068f1d1305a34a88
user <user@DESKTOP-8D7LR2I>
2023-03-09 11:59:10 +0800

    03091159

--- Web58/src/main/java/tw/brad/eeit58/Brad13.java
+++ Web58/src/main/java/tw/brad/eeit58/Brad13.java
@@ -11,8 +11,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import tw.brad.api.Bike;
+
 @WebServlet("/Brad13")
 public class Brad13 extends HttpServlet {
+	private Bike b1 = new Bike();
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		request.setCharacterEncoding("UTF-8");
@@ -22,6 +25,13 @@ public class Brad13 extends HttpServlet {
 			dispatcher = 
 				request.getRequestDispatcher(x.equals("brad")?"Brad14":"Brad15");
 		}
+		
+		b1.upSpeed(); b1.upSpeed(); b1.upSpeed(); b1.upSpeed();
+		System.out.println(b1.getSpeed());
+		
+		request.setAttribute("b1", b1);
+		
+		
 		//------------
 		
 		response.setContentType("text/html; charset=UTF-8");
@@ -32,7 +42,7 @@ public class Brad13 extends HttpServlet {
 		if (dispatcher!= null) dispatcher.forward(request, response);
 		out.print("<hr />");
 		out.print("Copyeight....");
-
+ 
 	
 	}
 
